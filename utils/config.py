5d65a1e53437c43a5dfdca5f457e0a66dda8524f
hs <haukesteffen@mbp.local>
2025-08-09 12:28:25 +0200

    S5E8 baseline

--- utils/config.py
+++ utils/config.py
@@ -1,37 +1,44 @@
 from pathlib import Path
-from pydantic import BaseModel
-import yaml
 from typing import Union
+
+import yaml
+from pydantic import BaseModel
+
 
 class Config(BaseModel):
     """General parameters.
-    
+
     Attributes:
         competition_name (str): Name of the Kaggle competition.
         data_path (Path): Path to the directory containing data files.
         submission_file (str): Name of the submission file.
+        target_column (str): Name of the target column in the dataset.
     """
+
     competition_name: str
     data_path: Path
     submission_file: str
+    target_column: str
+
 
 def _convert_paths(data: dict) -> dict:
     """Recursively convert all dictionary values containing 'path' to Path objects."""
     for key, value in data.items():
         if isinstance(value, dict):
             data[key] = _convert_paths(value)
-        elif isinstance(value, str) and 'path' in key.lower():
+        elif isinstance(value, str) and "path" in key.lower():
             data[key] = Path(value)
     return data
 
+
 def load_config(path: Union[str, Path]) -> Config:
     """Load configuration from YAML file."""
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         config = yaml.safe_load(f)
-    
+
     # Convert all 'path' strings to Path objects
     config = _convert_paths(config)
-    
+
     # Instantiate Config class
     config = Config(**config)
     print(f"Config loaded from {path}.")
--- utils/data.py
+++ utils/data.py
@@ -1,18 +1,18 @@
 import os
 import time
 import zipfile
+from typing import Tuple
+
 import kaggle
 import pandas as pd
-from typing import Tuple
 
 
 def download_competition_data(
-        competition_name: str, 
-        data_path: str
-    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    competition_name: str, data_path: str
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """
     Downloads the competition data from Kaggle, extracts it to the specified directory, returns a tuple of train and test DataFrames.
-    
+
     Args:
         competition_name (str): The name of the Kaggle competition.
         data_dir (str): The directory where the data will be saved.
@@ -23,32 +23,34 @@ def download_competition_data(
     # Ensure data directory exists
     if not os.path.exists(data_path):
         os.makedirs(data_path)
-    
+
     # Check if the competition data is already downloaded, else download it
     zip_file_path = os.path.join(data_path, f"{competition_name}.zip")
     if os.path.exists(zip_file_path):
-        print(f"Data for competition '{competition_name}' already exists at '{zip_file_path}'.")
+        print(
+            f"Data for competition '{competition_name}' already exists at '{zip_file_path}'."
+        )
     else:
         print(f"Downloading data for competition '{competition_name}'...")
-        kaggle.api.competition_download_files(competition=competition_name, path=data_path, quiet=True)
-    
+        kaggle.api.competition_download_files(
+            competition=competition_name, path=data_path, quiet=True
+        )
+
     # Extract the zip file
     zf = zipfile.ZipFile(zip_file_path)
-    with zf.open('train.csv') as f:
-        train = pd.read_csv(f)
-    with zf.open('test.csv') as f:
-        test = pd.read_csv(f)
+    with zf.open("train.csv") as f:
+        train = pd.read_csv(f).convert_dtypes()
+    with zf.open("test.csv") as f:
+        test = pd.read_csv(f).convert_dtypes()
     return train, test
-    
-    
+
+
 def upload_submission(
-        competition_name: str, 
-        data_dir: str,
-        submission_file: str
-    ) -> None:
+    competition_name: str, data_dir: str, submission_file: str
+) -> None:
     """
     Uploads the submission file to the specified Kaggle competition. After successfully submitting, it renames the file to avoid re-uploading.
-    
+
     Args:
         competition_name (str): The name of the Kaggle competition.
         data_dir (str): The directory where the submission file is located.
@@ -60,8 +62,14 @@ def upload_submission(
     # Ensure the submission file exists
     submission_path = os.path.join(data_dir, submission_file)
     if not os.path.exists(submission_path):
-        raise FileNotFoundError(f"Submission file '{submission_file}' does not exist in '{data_dir}'.")
+        raise FileNotFoundError(
+            f"Submission file '{submission_file}' does not exist in '{data_dir}'."
+        )
 
     # Upload the submission file
     print(f"Uploading '{submission_file}' to competition '{competition_name}'...")
-    kaggle.api.competition_submit(file_name=submission_path, message="Initial submission", competition=competition_name)
+    kaggle.api.competition_submit(
+        file_name=submission_path,
+        message="Initial submission",
+        competition=competition_name,
+    )
