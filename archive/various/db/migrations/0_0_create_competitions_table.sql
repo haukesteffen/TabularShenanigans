be65c20ddcac90677ab66dfa732828d51fbdeb01
hs <acc+github@haukesteffen.com>
2026-02-27 11:01:52 +0100

    cleanup

--- archive/various/db/migrations/0_0_create_competitions_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS competitions (
-    competition_id   INTEGER PRIMARY KEY AUTOINCREMENT,
-    name             TEXT NOT NULL,
-    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
